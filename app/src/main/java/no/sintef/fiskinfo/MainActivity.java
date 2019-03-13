c8beb501f884a1a9595e2f24c0b4f701992d51e8
erlendstav <erlend.stav@gmail.com>
2019-03-13 20:36:40 +0100

    Navigation works better, but should consider top level fragments

--- app/src/main/java/no/sintef/fiskinfo/MainActivity.java
+++ app/src/main/java/no/sintef/fiskinfo/MainActivity.java
@@ -1,5 +1,6 @@
 package no.sintef.fiskinfo;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,6 +9,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -20,9 +22,13 @@ import com.google.android.material.navigation.NavigationView;
 import java.util.Arrays;
 import java.util.HashSet;
 
+import static androidx.navigation.ui.NavigationUI.setupActionBarWithNavController;
+
 public class MainActivity extends AppCompatActivity {
 
     AppBarConfiguration appBarConfiguration = null;
+    NavController controller;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,25 +38,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        NavHostFragment host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
-        NavController controller = host.getNavController();
 
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBar actionBar = getSupportActionBar();
+        assert actionBar != null;
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_launcher_background);
+
+
+
+        NavHostFragment host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+        controller = host.getNavController();
+
+        drawerLayout = findViewById(R.id.drawer_layout);
 
         HashSet<Integer> topLevelDests = new HashSet<>();
         topLevelDests.add(R.id.map_fragment);
         topLevelDests.add(R.id.overview_fragment);
         topLevelDests.add(R.id.tools_fragment);
 
-        appBarConfiguration = new AppBarConfiguration.Builder(topLevelDests) //controller.getGraph())
+//        appBarConfiguration = new AppBarConfiguration.Builder(topLevelDests) //controller.getGraph())
+        appBarConfiguration = new AppBarConfiguration.Builder(controller.getGraph())
                 .setDrawerLayout(drawerLayout)
                 .build();
 
-        NavigationUI.setupActionBarWithNavController(this, controller, appBarConfiguration);
+        setupActionBarWithNavController(this, controller, drawerLayout);
+        //setupActionBarWithNavController(this, controller, appBarConfiguration);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawerLayout, toolbar, R.string.proto_open_drawer, R.string.proto_close_drawer);
-        toggle.syncState();
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//                this, drawerLayout, toolbar, R.string.proto_open_drawer, R.string.proto_close_drawer);
+//        toggle.syncState();
 
         setupNavigationMenu(controller);
 
@@ -66,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_nav_drawer, menu);
         return true;
-*        boolean retVal = super.onCreateOptionsMenu(menu);
+        boolean retVal = super.onCreateOptionsMenu(menu);
         NavigationView navView = findViewById(R.id.navigation_view);
         if (navView == null) {
             getMenuInflater().inflate(R.menu.menu_nav_drawer, menu);
@@ -82,13 +98,41 @@ public class MainActivity extends AppCompatActivity {
 */
     @Override
     public boolean onSupportNavigateUp() {
-        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
+        return NavigationUI.navigateUp(controller, drawerLayout);
+        //return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp();
     }
 
     protected void setupNavigationMenu(NavController navController) {
         NavigationView view = findViewById(R.id.navigation_view);
         NavigationUI.setupWithNavController(view, navController);
 
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_nav_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return NavigationUI.onNavDestinationSelected(item, controller)
+                || super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 
 }
