5453f64e24d5947a2b86e666f04aad2bcc379022
Erlend Stav <erlend.stav@gmail.com>
2019-08-02 14:28:51 +0200

    Added license and copyright information to the java source files, and added comments for a few classes.

--- app/src/main/java/no/sintef/fiskinfo/ui/snap/SnapFragment.java
+++ app/src/main/java/no/sintef/fiskinfo/ui/snap/SnapFragment.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (C) 2019 SINTEF
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package no.sintef.fiskinfo.ui.snap;
 
 import androidx.fragment.app.FragmentManager;
@@ -18,6 +33,11 @@ import com.google.android.material.tabs.TabLayout;
 
 import no.sintef.fiskinfo.R;
 
+/**
+ * The SnapFragment is the main container fragment for the echo snap message functionality.
+ * It contains tabs for showing the users own echo snaps, and for the inbox and outbox for
+ * snap messages.
+ */
 public class SnapFragment extends Fragment  {
 
     ViewPager viewPager;
