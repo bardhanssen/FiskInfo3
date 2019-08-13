da6633f3fa802fddc658c480c93934ae95696b2d
Erlend Stav <erlend.stav@gmail.com>
2019-08-13 16:56:48 +0200

    Started to add code to login fragment

--- app/src/main/java/no/sintef/fiskinfo/ui/login/LoginFragment.kt
+++ app/src/main/java/no/sintef/fiskinfo/ui/login/LoginFragment.kt
@@ -6,6 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.snackbar.Snackbar
 
 import no.sintef.fiskinfo.R
 
@@ -15,14 +21,50 @@ class LoginFragment : Fragment() {
         fun newInstance() = LoginFragment()
     }
 
-    private lateinit var viewModel: LoginViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.login_fragment, container, false)
     }
+
+
+    private val viewModel: LoginViewModel by activityViewModels()
+
+    private lateinit var usernameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        usernameEditText = view.findViewById(R.id.username_edit_text)
+        passwordEditText = view.findViewById(R.id.password_edit_text)
+
+        loginButton = view.findViewById(R.id.login_button)
+        loginButton.setOnClickListener {
+            viewModel.authenticate(usernameEditText.text.toString(),
+                passwordEditText.text.toString())
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            viewModel.refuseAuthentication()
+            navController.popBackStack(R.id.main_fragment, false)
+        })
+
+        val navController = findNavController()
+        viewModel.authenticationState.observe(viewLifecycleOwner, Observer { authenticationState ->
+            when (authenticationState) {
+                AUTHENTICATED -> navController.popBackStack()
+                INVALID_AUTHENTICATION ->
+                    Snackbar.make(view,
+                        R.string.invalid_credentials,
+                        Snackbar.LENGTH_SHORT
+                    ).show()
+            }
+        })
+    }
+
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
