45f065884848d9548f6586c21b0458565a0ba863
Erlend Stav <erlend.stav@gmail.com>
2019-09-04 17:07:04 +0200

    Adjusted some api paths to resent changes in api, and added check on initalization of outbox before send of snaps

---
+++ app/src/main/java/no/sintef/fiskinfo/ui/analysis/AnalysisFragment.kt
@@ -0,0 +1,54 @@
+package no.sintef.fiskinfo.ui.analysis
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
+
+import no.sintef.fiskinfo.R
+
+class AnalysisFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AnalysisFragment()
+    }
+
+    private lateinit var viewModel: AnalysisViewModel
+    private lateinit var webView: WebView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.analysis_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(AnalysisViewModel::class.java)
+        // TODO: Use the ViewModel
+        configureWebView()
+    }
+
+    fun configureWebView() {
+        if (getView() == null) return
+        webView = getView()!!.findViewById(R.id.map_fragment_web_view)
+        with (webView.settings) {
+            javaScriptEnabled = true
+            javaScriptCanOpenWindowsAutomatically = true
+            domStorageEnabled = true
+            setGeolocationEnabled(true)
+            layoutAlgorithm = WebSettings.LayoutAlgorithm.NARROW_COLUMNS
+        }
+        //webView.webViewClient =
+//        webView.addJavascriptInterface(WebAppInterface(context!!, loginViewModel),"Android" )
+        webView.setWebViewClient(BarentswatchFiskInfoWebClient())
+        webView.loadUrl("https://www.fangstanalyse.no")
+    }
+
+
+}
