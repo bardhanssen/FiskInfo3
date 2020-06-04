ba68fd75587d64f3e19e73e1cb3be9d5d64653f1
Erlend Stav <erlend.stav@gmail.com>
2020-06-04 17:39:01 +0200

    Some additions for tool details

--- app/src/main/java/no/sintef/fiskinfo/model/fishingfacility/ToolTypeCode.kt
+++ app/src/main/java/no/sintef/fiskinfo/model/fishingfacility/ToolTypeCode.kt
@@ -1,19 +1,25 @@
 package no.sintef.fiskinfo.model.fishingfacility
 
+import android.content.Context
 import com.google.gson.annotations.SerializedName
+import no.sintef.fiskinfo.R
 
 // TODO: enum [NETS, LONGLINE, CRABPOT, DANPURSEINE]
 
-enum class ToolTypeCode(val code : String) {
+enum class ToolTypeCode(val code : String, val stringResource : Int) {
     @SerializedName("NETS")
-    NETS("NETS"),
+    NETS("NETS", R.string.tool_type_code_nets),
 
     @SerializedName("LONGLINE")
-    LONGLINE("LONGLINE"),
+    LONGLINE("LONGLINE", R.string.tool_type_code_longline),
 
     @SerializedName("CRABPOT")
-    CRABPOT("CRABPOT"),
+    CRABPOT("CRABPOT", R.string.tool_type_code_crabpot),
 
     @SerializedName("DANPURSEINE")
-    DANPURSEINE("DANPURSEINE")
+    DANPURSEINE("DANPURSEINE", R.string.tool_type_code_danpurseine);
+
+    public fun getLocalizedName(context : Context):String {
+        return context.resources.getString(stringResource)
+    }
 }
--- app/src/main/java/no/sintef/fiskinfo/ui/tools/ToolDetailsFragment.kt
+++ app/src/main/java/no/sintef/fiskinfo/ui/tools/ToolDetailsFragment.kt
@@ -1,17 +1,21 @@
 package no.sintef.fiskinfo.ui.tools
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.Filter
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import no.sintef.fiskinfo.R
 import no.sintef.fiskinfo.databinding.ToolDetailsFragmentBinding
+import no.sintef.fiskinfo.model.fishingfacility.ToolTypeCode
 
 
 class ToolDetailsFragment : Fragment() {
@@ -25,6 +29,37 @@ class ToolDetailsFragment : Fragment() {
     private lateinit var mViewModel: ToolsViewModel
     private var mBinding: ToolDetailsFragmentBinding? = null
 
+    class MyArrayAdapter(context: Context?, resource: Int, objects: Array<ToolTypeCode>) :
+        ArrayAdapter<ToolTypeCode>(context, resource, objects) {
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+            val theView : TextView = super.getView(position, convertView, parent) as TextView
+            theView.text = getItem(position).getLocalizedName(context)
+            return theView
+        }
+
+        private val filter_that_does_nothing = object: Filter() {
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+                val results = FilterResults()
+                results.values = objects
+                results.count = objects.size
+                return results
+            }
+            override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+                notifyDataSetChanged()
+            }
+        }
+
+        override fun getFilter(): Filter {
+            return filter_that_does_nothing
+        }
+/*        override fun getFilter(): Filter {
+            return filter_that_does_nothing
+        }*/
+
+    }
+
+    private lateinit var mToolCodeAdapter : MyArrayAdapter
+    private lateinit var mEditTextFilledExposedDropdown: AutoCompleteTextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,8 +67,11 @@ class ToolDetailsFragment : Fragment() {
     ): View? {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.tool_details_fragment, container, false)
 
-        val TOOLS =
-            arrayOf("NETS", "LONGLINE", "CRABPOT", "DANPURSEINE")
+        //ToolTypeCode.values()
+
+        //val TOOLS =
+
+        /*    arrayOf("NETS", "LONGLINE", "CRABPOT", "DANPURSEINE")
 
         val adapter = ArrayAdapter(
             context,
@@ -41,10 +79,12 @@ class ToolDetailsFragment : Fragment() {
             //R.array.tool_type_codes
             TOOLS
         )
+        */
+        mToolCodeAdapter = MyArrayAdapter(context, R.layout.exposed_dropdown_menu_item, ToolTypeCode.values())
 
-        val editTextFilledExposedDropdown: AutoCompleteTextView =
+        mEditTextFilledExposedDropdown =
             mBinding!!.root.findViewById(R.id.toolDetailsTypeField)
-        editTextFilledExposedDropdown.setAdapter(adapter)
+        mEditTextFilledExposedDropdown.setAdapter(mToolCodeAdapter)
 
 
 
@@ -58,6 +98,8 @@ class ToolDetailsFragment : Fragment() {
         mViewModel.getSelectedTool().observe(this, Observer { tool ->
             if (tool != null) {
                 mBinding!!.setTool(tool)
+                if (context != null)
+                    mEditTextFilledExposedDropdown.setText(tool.toolTypeCode?.getLocalizedName(context!!), false)
                 //mBinding!!.setEchogram(mViewModel?.draftMetadata)
                 //mBinding!!.setHandlers(this@SnapEditorFragment)
                 //mBinding!!.setSnapviewmodel(mViewModel)
