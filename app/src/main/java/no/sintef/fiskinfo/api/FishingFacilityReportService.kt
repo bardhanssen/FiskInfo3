62832dc8c53128eacfd81edaa3ce670b33820fb2
Erlend Stav <erlend.stav@gmail.com>
2020-09-30 15:31:08 +0200

    Tool retrieval now working

--- app/src/main/java/no/sintef/fiskinfo/api/FishingFacilityReportService.kt
+++ app/src/main/java/no/sintef/fiskinfo/api/FishingFacilityReportService.kt
@@ -6,10 +6,7 @@ import no.sintef.fiskinfo.model.fishingfacility.FishingFacilityChanges
 import no.sintef.fiskinfo.model.fishingfacility.FiskInfoProfileDTO
 import no.sintef.fiskinfo.model.fishingfacility.RetrievalInfoDto
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
 
 // Documentation of API:  https://code.barentswatch.net/openapi/?url=https://www.barentswatch.no/api/documentation/fiskinfo-reporting.yaml
 
@@ -20,7 +17,7 @@ interface FishingFacilityReportService {
         const val fishingfacilityprofile = v1prefix + "fishingfacilityprofile"
         const val fishingfacilitychanges = v2prefix + "fishingfacilitychanges"
         const val deployed = v1prefix + "fishingfacilitychange/deployed"
-        const val retrieved = v1prefix + "fishingfacilitychange/retrieved"
+        const val retrieved = v1prefix + "fishingfacilitychange/retrieved/{toolid}"
         //const val geoDataSubscription = prefix + "subscription/"
         //const val geoDataSubscriptionManagement = prefix + "subscription/{Id}"
         //const val geoDataDownload = prefix + "download/{ApiName}"
@@ -38,7 +35,7 @@ interface FishingFacilityReportService {
     fun sendDeploymentInfo(@Body deploymentInfo: DeploymentInfo):Call<Void?> // Call<JsonElement?>
 
     @POST(retrieved)
-    fun sendRetrieved(@Query("toolId") toolId: String, @Body retrievalInfo: RetrievalInfoDto):Call<Void?>
+    fun sendRetrieved(@Path("toolid") toolId: String, @Body retrievalInfo: RetrievalInfoDto):Call<Void?>
 
     /*
     @GET(geoDataSubscription)
