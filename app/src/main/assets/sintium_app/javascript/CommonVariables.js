107c06bda8cfca0f114dc001c72a44c4a23adc15
Erlend Stav <erlend.stav@gmail.com>
2020-11-24 11:23:25 +0100

    Removed old copy of sintium app

--- app/src/main/assets/sintium_app/javascript/CommonVariables.js
+++
@@ -1,9 +0,0 @@
-let selectedFeature;
-let selectedKey = null;
-
-const tokenApiURL = "https://www.barentswatch.no/api/token";
-const token = Android.getToken();
-const authenticator = Sintium.OAuthAuthenticator(tokenApiURL, token);
-const unrollAtZoom = 12;
-
-vesselMap = {};
