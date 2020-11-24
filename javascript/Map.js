e11881358c62a69854fbc6762e3c9267a956b4e3
Erlend Stav <erlend.stav@gmail.com>
2020-11-24 11:23:37 +0100

    Squashed 'app/src/main/assets/sintium_app/' content from commit b0255e2
    git-subtree-dir: app/src/main/assets/sintium_app
    git-subtree-split: b0255e2969e00a88997a960441b735327ac15b73

---
+++ javascript/Map.js
@@ -0,0 +1,44 @@
+function closeInfoDrawer(e) {
+    e.getMap().getSelectionHandler().clearSelection();
+    infoDrawer.close();
+    //vesselInfoDrawer.close();
+}
+
+// Instantiating layer switcher
+
+const zoomControl = Sintium.zoomControl();
+
+const layerSwitcher = Sintium.sidebarLayerSwitcher({
+    position: "left",
+    dataLayerLabel: 'Datalag',
+    baseLayerLabel: 'Kartlag'
+});
+
+const seaMapLayer = Sintium.wmsLayer({
+    layerId: "Sjøkart",
+    url: "https://opencache.statkart.no/gatekeeper/gk/gk.open?",
+    params: {
+        LAYERS: 'sjokartraster',
+        VERSION: '1.1.1'
+    },
+    visible: true
+});
+
+// Instantiating map
+const map = Sintium.map({
+    domId: "map",
+    layers: [seaBottomInstallationsLayer, maritimeBordersLayer, fishRegulationsGroup, seismicGroup, iceGroup, tradeAreaGroup, seaMapLayer],
+    use: [infoDrawer, layerSwitcher],
+    controls: [zoomControl],
+    zoomOnClusterClick: true,
+    onClickEmptySpace: closeInfoDrawer,
+    onClickCluster: closeInfoDrawer,
+    hitTolerance: 4,
+    rotation: false
+});
+
+function addToolsAndVesselsLayerToMap() {
+    map.layer(toolsLayer);
+    map.layer(vesselsLayer);
+    map.use(vesselInfoDrawer);
+}
